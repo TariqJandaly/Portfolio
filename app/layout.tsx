7f7d8685be74318cef78d7905d2470e3701fb088
Tariq Jandaly <tariq.jandaly@gmail.com>
2024-06-18 22:47:25 +0300

    Changing the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link rel="icon" href="https://fav.farm/🐱‍👤" />
+      </head>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
