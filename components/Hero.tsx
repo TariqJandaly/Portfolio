8bccf32bdd93d3625ab56155b650241a34a2c828
Tariq Jandaly <tariq.jandaly@gmail.com>
2024-06-19 17:56:45 +0300

    Fixing the buttons on the mobile screen doesnt work

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero: React.FC<HeroProps> = ({}) => {
     <main id="hero" className="w-screen h-screen relative">
       <div className="flex items-center w-full h-full bg-cover bg-center" style={{ backgroundImage: "url(main-bg.webp)" }}>
         <div className="flex flex-col gap-5 z-50 max-w-[750px] pl-5 md:pl-40 pb-56 md:pb-20">
-          <h1 className="text-[50px] text-white font-semibold text-justify">
+          <h1 className="text-[50px] text-white font-semibold md:text-justify">
             Make anything possible with
             <span className="text-transparent bg-clip-text bg-gradient-to-r  from-purple-500 to-cyan-500 animate-gradient">
               {" "}
@@ -41,7 +41,7 @@ const Hero: React.FC<HeroProps> = ({}) => {
       </div>
 
 
-      <div className="absolute flex bottom-20 z-[20] right-5 flex-col md:hidden gap-5">
+      <div className="absolute flex bottom-20 z-50 right-5 flex-col md:hidden gap-5">
         <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-purple-800">
             <Link href={'#more'} className="w-full relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
                 Learn more
